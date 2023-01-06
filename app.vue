67f01d5088c68cf02f58e505cf9c8296c4399672
Samit Koyom <samitkoyom@gmail.com>
2023-01-06 15:29:00 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex h-screen">
+    <v-card class="mx-auto my-auto" width="400" prepend-icon="mdi-home">
+      <template v-slot:title>
+        Hello Vuetify 3
+      </template>
+      <v-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic accusantium officia laudantium dignissimos tenetur accusamus, corporis, quas rerum eum perferendis eligendi voluptates consectetur nam odit id ad esse! Recusandae.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn variant="text" color="teal-accent-4">
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
